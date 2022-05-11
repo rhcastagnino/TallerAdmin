b9ff0a98cab4d5b7b2975493f97f9620cdb7ecd7
Rodrigo Castagnino <rhcastagnino@gmail.com>
2022-05-11 20:15:12 -0300

    Cambios

---
+++ DAL/Acceso.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Acceso : Conexion
+    {
+        SqlConnection connection = new SqlConnection();
+
+        private void Conectar()
+        {
+            connection.ConnectionString = conexion;
+            connection.Open();
+        }
+        private void Desconectar()
+        {
+            connection.Close();
+        }
+
+        public DataTable executeNonQuery(string query)
+        {
+            SqlDataReader dr;
+            DataTable dt = new DataTable();
+            Conectar();
+            SqlTransaction TR;
+            SqlCommand Comando = new SqlCommand(query, connection);
+            TR = connection.BeginTransaction();
+
+            try
+            {
+                Comando.Transaction = TR;
+                dr = Comando.ExecuteReader();
+                dt.Load(dr);
+                TR.Commit();
+            }
+            catch (Exception)
+            {
+                TR.Rollback();
+            }
+
+            Desconectar();
+            return dt;
+        }
+    }
+}
---
+++ DAL/Conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+
+namespace DAL
+{
+    public class Conexion
+    {
+        private readonly string _server;
+        private readonly string _base;
+        public string conexion { get; set; }
+        public Conexion()
+        {
+            _server = ConfigurationManager.AppSettings["server"];
+            _base = ConfigurationManager.AppSettings["base"];
+
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = _server,
+                InitialCatalog = _base,
+                IntegratedSecurity = true,
+            };
+            conexion = sqlConnectionStringBuilder.ConnectionString;
+        }
+    }
+}
